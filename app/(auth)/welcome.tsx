f9ca108a39073b67cb82daccaebc4c1c5f5bba1c
Vince Dority <vncdority@gmail.com>
2024-12-07 07:36:01 -0500

    welcome page updated to correctly  route to sign-up form

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,10 +1,22 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+import Swiper from "react-native-swiper";
+import { useRef } from "react";
 
 const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
   return (
-    <SafeAreaView className="flex-1  justify-center bg-white">
-      <Text className="w-full  text-center  text-blue-700">Uber Clone</Text>
+    <SafeAreaView className="flex h-full items-center justify-between bg-white ">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+      <Swiper ref={swiperRef}></Swiper>
     </SafeAreaView>
   );
 };
