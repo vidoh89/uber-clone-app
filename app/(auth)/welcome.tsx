b24e882da20fb79c9289e57a42c580b275719c8e
Vince Dority <vncdority@gmail.com>
2024-12-10 13:29:14 -0500

    onboarding component created and placed in constants folder.

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -2,12 +2,17 @@ import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { router } from "expo-router";
 import Swiper from "react-native-swiper";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { onboarding } from "@/constants";
 
 const Onboarding = () => {
+  // Swiper component
   const swiperRef = useRef<Swiper>(null);
+  //Keeps track of the swiperRef's index
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white ">
+      {/*Button: configurations  for the sign-up page:gives the ability to skip*/}
       <TouchableOpacity
         onPress={() => {
           router.replace("/(auth)/sign-up");
@@ -16,7 +21,24 @@ const Onboarding = () => {
       >
         <Text className="text-black text-md font-JakartaBold">Skip</Text>
       </TouchableOpacity>
-      <Swiper ref={swiperRef}></Swiper>
+      {/*Create a reference for swiper component*/}
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full " />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full " />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View>
+            <Text>{item.description}</Text>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 };
---
+++ constants/index.ts
@@ -0,0 +1,9 @@
+//Onbording objects
+export const onboarding = [
+  {
+    id: 1,
+    title: "Explore the world with confidence",
+    description: "Don't just share a ride, share memories!",
+    image: null,
+  },
+];
